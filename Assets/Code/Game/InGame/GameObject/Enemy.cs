2a788e7f663449bfd45641bdfe989b521c4b307d
刘峰 <zgjllf@msn.cn>
2018-03-20 23:25:40 +0800

    no message

--- Assets/Code/Game/InGame/GameObject/Enemy.cs
+++ Assets/Code/Game/InGame/GameObject/Enemy.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Enemy : BaseObject {
 
     public float speed = 0;
-
+    Vector3 baseScale;
     public override void ObjInit()
     {
         base.ObjInit();
         speed = Random.Range(0f, 1f);
+        baseScale = transform.localScale;
     }
 
     public override void ObjUpdate()
     {
         if (state == enObjState.die) return;
-        transform.position = transform.position - new Vector3(0, 0,GetSpeed() * Time.deltaTime);
+        Vector3 pos = transform.position - new Vector3(0, 0, GetSpeed() * Time.deltaTime);
+
+        pos.y = Mathf.Abs(Mathf.Sin(transform.position.z*2)) *0.3f;
+
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler((pos.y - 0.1f)*90, 0, 0);
+        transform.localScale = baseScale + new Vector3(0,pos.y - 0.3f,0);
 
         if(transform.position.z < 0){
             state = enObjState.die;
